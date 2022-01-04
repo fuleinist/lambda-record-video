b6a4e712f74dff48955db1cb87fd75513c1741f5
fuleinist <fuleinist@gmail.com>
2022-01-05 10:45:57 +1100

    init

---
+++ test/lambda-record-video.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaRecordVideo from '../lib/lambda-record-video-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-record-video-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaRecordVideo.LambdaRecordVideoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
